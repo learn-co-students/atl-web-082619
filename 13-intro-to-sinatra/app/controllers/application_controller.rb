c39bf057452cce5dbb9b1fc38e47d883d5524613
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-09-16 13:18:51 -0400

    post lecture 13a

--- 13-intro-to-sinatra/app/controllers/application_controller.rb
+++ 13-intro-to-sinatra/app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < Sinatra::Base
 
+    set :views, "app/views"
+
+    books = [{title: "Harry Potter and the Sorcerer's Stone",
+               author: "J.K. Rowling",
+               snippet: "Harry beats a troll"},
+               {title: "Harry Potter and the Chamber of Secrets",
+                author: "J.K. Rowling",
+                snippet: "Harry confronts racism"}]
+
+
+    get "/" do
+        # "<h1>Hello Atlanta!!!!!!</h1>"
+        erb :home
+    end
+
+    get "/books" do 
+        # @my_name = "Paul"
+        @books = books # Book.all
+        erb :index 
+    end
+
 end

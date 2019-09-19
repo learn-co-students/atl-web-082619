997b8942f449a83878e95cb3a8e62e78ba4214aa
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-09-19 11:02:49 -0400

    post lecture

--- 16-sinatra-associated-objects-cont/app/controllers/authors_controller.rb
+++ 16-sinatra-associated-objects-cont/app/controllers/authors_controller.rb
@@ -8,8 +8,26 @@ class AuthorsController < Sinatra::Base
         erb :index
     end
 
+    get "/authors/new" do 
+        erb :new
+    end
+
     get "/authors/:id" do 
         @author = Author.find(params[:id])
         erb :show
     end
+
+    post "/authors" do 
+        author = Author.find_or_create_by(name: params[:name])
+        params[:book].each do |book_hash|
+            if !book_hash[:title].empty?
+                book_title = book_hash[:title]
+                book_snippet = book_hash[:snippet]
+                Book.create(
+                    author: author, snippet: book_snippet, title: book_title)
+            end
+        end
+        redirect "/authors/#{author.id}"
+    end
+
 end
